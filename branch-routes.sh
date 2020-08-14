0f3caa7ab70dd640b276bb4f11533ac91d2b41e8
mddazure <mddazure@gmail.com>
2020-08-14 14:11:58 +0200

    branch vpn script

--- branch-routes.sh
+++ branch-routes.sh
@@ -1,10 +1,11 @@
 hubgwbgpaddress=$(az network vpn-gateway show --name microhack-we-hub-vng  -g vwan-microhack-hub-rg --query "bgpSettings.bgpPeeringAddresses[?ipconfigurationId == 'Instance0'].defaultBgpIpAddresses" --output tsv)
+echo "Hub GW BGP address:" $hubgwbgpaddress
 
-# VNETGW: Verify BGP peer status
+echo # VNETGW: Verify BGP peer status
 az network vnet-gateway list-bgp-peer-status -n vnet-gw-onprem -g vwan-microhack-spoke-rg
 
-# VNETGW: Display routes advertised from onprem gw to hub
+echo # VNETGW: Display routes advertised from onprem gw to hub
 az network vnet-gateway list-advertised-routes -n vnet-gw-onprem -g vwan-microhack-spoke-rg --peer $hubgwbgpaddress
 
-# VNETGW: Display routes learned by onprem gw from hub
+echo # VNETGW: Display routes learned by onprem gw from hub
 az network vnet-gateway list-learned-routes -n vnet-gw-onprem -g vwan-microhack-spoke-rg
--- connect-branch.sh
+++ connect-branch.sh
@@ -1,25 +1,31 @@
 az extension add --name virtual-wan
 
-# VNETGW: Get parameters from onprem gateway
+echo "# VNETGW: Get parameters from onprem gateway"
 vnetgwtunnelip=$(az network vnet-gateway show -n vnet-gw-onprem -g vwan-microhack-spoke-rg --query "bgpSettings.bgpPeeringAddresses[0].tunnelIpAddresses[0]" --output tsv)
+echo "VNET GW Tunnel address:" $vnetgwtunnelip
 vnetgwbgpip=$(az network vnet-gateway show -n vnet-gw-onprem -g vwan-microhack-spoke-rg --query "bgpSettings.bgpPeeringAddress" --output tsv)
+echo "VNET GW BGP address:" $vnetgwbgpip
 vnetgwasn=$(az network vnet-gateway show -n vnet-gw-onprem -g vwan-microhack-spoke-rg --query "bgpSettings.asn" --output tsv)
+echo "VNET GW BGP ASN:" $vnetgwasn
 sharedkey="m1cr0hack"
 
-# VWAN: Create remote site
+echo "# VWAN: Create remote site"
 az network vpn-site create --ip-address $vnetgwtunnelip --name onprem -g vwan-microhack-hub-rg --asn $vnetgwasn --bgp-peering-address $vnetgwbgpip --virtual-wan microhack-vwan --location northeurope
 
-# VWAN: Create connection - remote site to hub gw
+echo "# VWAN: Create connection - remote site to hub gw"
 az network vpn-gateway connection create --gateway-name microhack-we-hub-vng --name onprem --remote-vpn-site onprem -g vwan-microhack-hub-rg --shared-key $sharedkey --enable-bgp true
 
-# VWAN: Get parameters from VWAN Hub GW
+echo "# VWAN: Get parameters from VWAN Hub GW"
 hubgwtunneladdress=$(az network vpn-gateway show --name microhack-we-hub-vng  -g vwan-microhack-hub-rg --query "bgpSettings.bgpPeeringAddresses[?ipconfigurationId == 'Instance0'].tunnelIpAddresses[0]" --output tsv)
+echo "Hub GW Tunnel address:" $hubgwtunneladdress
 hubgwbgpaddress=$(az network vpn-gateway show --name microhack-we-hub-vng  -g vwan-microhack-hub-rg --query "bgpSettings.bgpPeeringAddresses[?ipconfigurationId == 'Instance0'].defaultBgpIpAddresses" --output tsv)
+echo "Hub GW BGP address:" $hubgwbgpaddress
 hubgwasn=$(az network vpn-gateway show --name microhack-we-hub-vng  -g vwan-microhack-hub-rg --query "bgpSettings.asn" --output tsv)
+echo "Hub GW BGP ASN:" $hubgwasn
 hubgwkey=$(az network vpn-gateway connection show --gateway-name microhack-we-hub-vng --name onprem -g vwan-microhack-hub-rg --query "sharedKey" --output tsv)
 
-# LNG
+echo "# create local network gateway"
 az network local-gateway create -g vwan-microhack-spoke-rg -n lng --gateway-ip-address $hubgwtunneladdress --location westeurope --asn $hubgwasn --bgp-peering-address $hubgwbgpaddress
 
-# VNET GW: connect from vnet gw to local network gateway
+echo "# VNET GW: connect from vnet gw to local network gateway"
 az network vpn-connection create -n to-we-hub --vnet-gateway1 vnet-gw-onprem -g vwan-microhack-spoke-rg --local-gateway2 lng -l northeurope --shared-key $sharedkey --enable-bgp
