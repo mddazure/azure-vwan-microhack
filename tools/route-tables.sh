83e842d81e2d33c911ac7c8d58e72f9b20613500
HolgerR <48099512+HolgerReiners@users.noreply.github.com>
2020-12-22 16:01:57 +0100

    Revert "adding tools"
    This reverts commit b3a0522c15ac66a55a447aca281f6ffcd968d4c1.

--- tools/route-tables.sh
+++
@@ -1,9 +0,0 @@
-## Scenario 1 / Task 1
-az network nic show-effective-route-table -g vwan-microhack-spoke-rg -n spoke-1-nic --output table
-# PoSh - Get-AzEffectiveRouteTable -ResourceGroupName vwan-microhack-spoke-rg -NetworkInterfaceName spoke-1-nic | ft
-
-## Scenario 1 / Task 2
-az network nic show-effective-route-table -g vwan-microhack-spoke-rg -n spoke-2-nic --output table
-# PoSh - Get-AzEffectiveRouteTable -ResourceGroupName vwan-microhack-spoke-rg -NetworkInterfaceName spoke-2-nic | ft
-
-## Scenario 2 / Task 1
