b3f6ca5e30247a59f60128efc46c007d674518e4
mddazure <mddazure@gmail.com>
2021-01-31 15:49:24 +0100

    update iptables config on nva

--- enable-routing-nva.sh
+++ enable-routing-nva.sh
@@ -1,4 +1,7 @@
 sudo chmod 777 /etc/sysctl.conf
 echo "net.ipv4.ip_forward = 1" > /etc/sysctl.conf
 sudo sysctl -p /etc/sysctl.conf
-# sudo  iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -d 10.0.0.0/8 -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -d 172.16.0.0/12 -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -d 192.168.0.0/16 -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
