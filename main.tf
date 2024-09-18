ba910b22cc4e6a02732aabe7b7ae074019919b0e
Marc de Droog <madedroo@microsoft.com>
2024-09-18 11:22:14 +0200

    version

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.0.1"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
+
+
 #######################################################################
 ## Create Resource Group
 #######################################################################
