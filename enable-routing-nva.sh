7287ffc9aa1895a1b92f0902fc57e831ec2a5e36
Marc de Droog <madedroo@microsoft.com>
2026-05-11 21:48:14 +0200

    iptablesupdate

--- enable-routing-nva.sh
+++ enable-routing-nva.sh
@@ -5,3 +5,6 @@ sudo iptables -t nat -A POSTROUTING -d 10.0.0.0/8 -j ACCEPT
 sudo iptables -t nat -A POSTROUTING -d 172.16.0.0/12 -j ACCEPT
 sudo iptables -t nat -A POSTROUTING -d 192.168.0.0/16 -j ACCEPT
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+sudo iptables -A FORWARD -i eth0 -o eth0 -j ACCEPT
+sudo iptables -A FORWARD -i eth0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
