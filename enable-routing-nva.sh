5d3758b882e0be00efddb969353fb4229e6597cc
Marc de Droog <madedroo@microsoft.com>
2026-05-12 11:26:41 +0200

    revert iptablesto single nic

--- enable-routing-nva.sh
+++ enable-routing-nva.sh
@@ -10,16 +10,5 @@ sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -i eth0 -o eth0 -j ACCEPT
 sudo iptables -A FORWARD -i eth0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 
-## dual interface config (eth0 outside, eth1 inside)
-sudo chmod 777 /etc/sysctl.conf
-echo "net.ipv4.ip_forward = 1" | sudo tee /etc/sysctl.d/99-ipforward.conf
-sudo sysctl -p /etc/sysctl.d/99-ipforward.conf
 
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-
-# Allow inside → outside
-sudo iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
-
-# Allow return traffic
-sudo iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-
+NB: dual nic config in linux is complex and not supported in this script. See https://learn.microsoft.com/en-us/troubleshoot/azure/virtual-machines/linux/linux-vm-multiple-virtual-network-interfaces-configuration
