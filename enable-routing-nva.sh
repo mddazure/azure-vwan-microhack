9cbda4ce1d73fc36f1ac23b29e2ebaae15f16cea
Marc de Droog <madedroo@microsoft.com>
2026-05-12 11:34:59 +0200

    revert_to_single_nic

--- enable-routing-nva.sh
+++ enable-routing-nva.sh
@@ -10,5 +10,6 @@ sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 sudo iptables -A FORWARD -i eth0 -o eth0 -j ACCEPT
 sudo iptables -A FORWARD -i eth0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 
+sudo iptables-save | sudo tee /etc/iptables/rules.v4 > /dev/null
 
 NB: dual nic config in linux is complex and not supported in this script. See https://learn.microsoft.com/en-us/troubleshoot/azure/virtual-machines/linux/linux-vm-multiple-virtual-network-interfaces-configuration
